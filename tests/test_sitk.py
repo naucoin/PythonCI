4d60f66a5556504e99d6a833f19f0a7022bcddca
Nicole Aucoin <nicole@bwh.harvard.edu>
2016-09-29 14:28:22 -0400

    ENH: Add testing
    Add basic nose tests to check if the continuous integration sites will work with and without SimpleITK.
    Add CI config files for Travis, Appveyor, Circle.

---
+++ tests/test_sitk.py
@@ -0,0 +1,31 @@
+# nosetests --nocapture -v tests/test_sitk.py
+
+import sys, os
+print('test_sitk: Python = %s, version = %s' % (sys.executable, sys.version))
+import logging
+from nose_parameterized import parameterized
+import SimpleITK as sitk
+from itertools import product
+
+testCases = ["A", "B"]
+features = ["one", "two", "three", "four"]
+
+class TestSITK:
+
+  def generate_scenarios():
+    global testCases
+    global features
+
+    for testCase in testCases:
+      for feature in features:
+        yield testCase, feature
+
+  @parameterized.expand(generate_scenarios())
+  def test_scenario(self, testCase, featureClassName):
+    print("")
+
+    logging.debug('test_scenario: testCase = %s, featureClassName = %s', testCase, featureClassName)
+
+def teardown_module():
+  print("")
+
